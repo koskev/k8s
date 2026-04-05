c99b10803957a42736c928a79b9b6162a3d81250
Kevin <kevin@kokev.de>
2026-04-05 16:21:17 +0200

    ci(github): Pin images

--- nix/actions.nix
+++ nix/actions.nix
@@ -4,8 +4,8 @@ let
     checkout = "actions/checkout@93cb6efe18208431cddfb8368fd83d5badbf9bfd"; # v5
   };
   container = {
-    grustonnet = "docker://ghcr.io/koskev/grustonnet-ls:v0.4.1";
-    tflint = "docker://ghcr.io/terraform-linters/tflint";
+    grustonnet = "docker://ghcr.io/koskev/grustonnet-ls@sha256:554f50e6be93023d696cea2b1f5a0378066f96cd9cb7055644674d71dc4c2e98"; # v0.4.1
+    tflint = "docker://ghcr.io/terraform-linters/tflint@sha256:b835d64d66abfdbc146694b918eb3cd733ec772465ad511464d4e8bebbdd6732";
   };
 
   steps = {
