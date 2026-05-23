16ed77435fd65070e3c89f752edc0ed77b7e4a8f
Kevin <kevin@kokev.de>
2026-05-23 16:40:17 +0200

    refactor(tf): Split state for openbao and authentik

--- tf/openbao/secrets.tf
+++ tf/openbao/secrets.tf
@@ -17,16 +17,9 @@ terraform {
       source = "hashicorp/kubernetes"
       version = "3.1.0"
     }
-    authentik = {
-      source = "goauthentik/authentik"
-      version = "2026.2.0"
-    }
   }
 }
 
-provider "authentik"  {
-    url   = "https://auth.kokev.de"
-}
 provider "vault" {}
 provider "sops" {}
 provider "kubernetes" {
@@ -208,76 +201,4 @@ resource "vault_pki_secret_backend_config_issuers" "config" {
   backend    = vault_mount.glusterfs_pki.path
   # TODO: Get from data source
   default    = "5099e2cf-1263-31c4-1c00-6a10f712cb62"
-}
-
-data "authentik_flow" "default-authorization-flow" {
-  slug = "default-provider-authorization-implicit-consent"
-}
-
-data "authentik_flow" "default-invalidation-flow" {
-  slug = "default-provider-invalidation-flow"
-}
-
-data "authentik_property_mapping_provider_scope" "profile" {
-  managed = "goauthentik.io/providers/oauth2/scope-profile"
-}
-
-data "authentik_property_mapping_provider_scope" "email" {
-  managed = "goauthentik.io/providers/oauth2/scope-email"
-}
-
-data "authentik_property_mapping_provider_scope" "openid" {
-  managed = "goauthentik.io/providers/oauth2/scope-openid"
-}
-
-data "authentik_certificate_key_pair" "generated" {
-  name = "authentik Self-signed Certificate"
-}
-
-data "sops_file" "argocd_secret" {
-  source_file = "openbao_secrets/argocd-secret.enc.yaml"
-}
-
-resource "authentik_provider_oauth2" "name" {
-  name               = "argocd"
-  client_id          = "argocd"
-  client_secret      = data.sops_file.argocd_secret.data["dex.authentik.clientSecret"]
-  authorization_flow = data.authentik_flow.default-authorization-flow.id
-  invalidation_flow   = data.authentik_flow.default-invalidation-flow.id
-  allowed_redirect_uris = [
-    {
-    "matching_mode" = "strict"
-    "url": "https://argocd.kokev.de/api/dex/callback"
-  },
-    {
-    "matching_mode" = "strict"
-    "url": "https://localhost:8085/auth/callback"
-  }
-  ]
-  property_mappings = [
-    data.authentik_property_mapping_provider_scope.profile.id,
-    data.authentik_property_mapping_provider_scope.email.id,
-    data.authentik_property_mapping_provider_scope.openid.id,
-  ]
-  signing_key = data.authentik_certificate_key_pair.generated.id
-}
-
-resource "authentik_application" "argocd" {
-  name              = "argocd"
-  slug              = "argocd"
-  protocol_provider = authentik_provider_oauth2.name.id
-}
-
-data "authentik_group" "admins" {
-  name = "authentik Admins"
-}
-
-resource "authentik_group" "argocd_admin" {
-  name = "ArgoCD Admins"
-}
-
-resource "authentik_user" "kevin" {
-  username = "kevin"
-  name     = "kevin"
-  groups   = [authentik_group.argocd_admin.id, data.authentik_group.admins.id]
 }
