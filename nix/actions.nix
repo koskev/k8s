e2f76405b166faf0c46cff4ba9f977b10ff2042b
Kevin <kevin@kokev.de>
2026-05-24 22:52:56 +0200

    ci: Update jsonnet linter

--- nix/actions.nix
+++ nix/actions.nix
@@ -4,7 +4,7 @@ let
     checkout = "actions/checkout@93cb6efe18208431cddfb8368fd83d5badbf9bfd"; # v5
   };
   container = {
-    grustonnet = "docker://ghcr.io/koskev/grustonnet-ls@sha256:554f50e6be93023d696cea2b1f5a0378066f96cd9cb7055644674d71dc4c2e98"; # v0.4.1
+    grustonnet = "docker://ghcr.io/koskev/grustonnet-ls:v0.6.3@sha256:f160447c45d184d660954b3b4e186e2d35bb1a4307321cdd043f6d83cd132d41";
     tflint = "docker://ghcr.io/terraform-linters/tflint@sha256:b835d64d66abfdbc146694b918eb3cd733ec772465ad511464d4e8bebbdd6732";
   };
 
