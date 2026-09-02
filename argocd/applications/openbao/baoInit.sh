4f4847389ce6c10be53935bb0a193de986af7474
Kevin <kevin@kokev.de>
2026-09-02 10:50:52 +0200

    feat(test): Add make target for kind cluster and fix openbao init

--- argocd/applications/openbao/baoInit.sh
+++ argocd/applications/openbao/baoInit.sh
@@ -1,10 +1,15 @@
 export BAO_ADDR=http://openbao:8200
-until bao status | grep "Initialized"; do
+
+BAO_STATUS=""
+until echo "$BAO_STATUS" | grep "Initialized"; do
 	echo "Waiting for openbao to start..."
 	sleep 5
+	BAO_STATUS=$(bao status)
 done
 
-if bao status | grep -q "Initialized.*false"; then
+echo "Bao status: ${BAO_STATUS}"
+
+if echo "$BAO_STATUS" | grep -q "Initialized.*false"; then
 	echo "Initialising OpenBao..."
 	bao operator init -key-shares=5 -key-threshold=3 \
 		-format=json > /shared/init.json
--- scripts/nft.sh
+++ scripts/nft.sh
@@ -4,9 +4,26 @@ sudo nft delete table ip6 nat_kind
 sudo nft add table ip6 nat_kind
 sudo nft add chain ip6 nat_kind output \{ type nat hook output priority filter \; \}
 
-podman ps -q --filter "label=io.x-k8s.cloud-provider-kind.loadbalancer.name=kind/ingress-traefik-external/ingress-traefik-external" --format json | jq -r '.[].Ports[] | select(.container_port == 80 or .container_port == 443) | "\(.container_port) \(.host_port)"' | while read -r c_port h_port; do
-    if [ -n "$h_port" ] && [ "$h_port" != "null" ]; then
-        echo "Adding rule for $h_port -> $c_port"
-        sudo nft add rule ip6 nat_kind output oifname "lo" tcp dport "$c_port" redirect to :"$h_port"
-    fi
-done
+
+function get_ports() {
+    CONTAINER_PORT=$1
+    PORTS=$(podman ps -q --filter "label=io.x-k8s.cloud-provider-kind.loadbalancer.name=kind/ingress-traefik-external/ingress-traefik-external" --format json | jq -r ".[].Ports[] | select(.container_port == ${CONTAINER_PORT}) | \"\(.container_port) \(.host_port)\"")
+    echo "$PORTS"
+}
+
+function add_rule() {
+    CONTAINER_PORT=$1
+    c_port="null"
+    h_port="null"
+    until [ "$c_port" != "null" ] && [ "$h_port" != "null" ]; do
+	echo "Checking for ingress ports..."
+	PORTS=$(get_ports "$CONTAINER_PORT")
+	read -r c_port h_port <<< "$PORTS"
+    done
+
+    echo "Adding rule for $h_port -> $c_port"
+    sudo nft add rule ip6 nat_kind output oifname "lo" tcp dport "$c_port" redirect to :"$h_port"
+}
+
+add_rule 80
+add_rule 443
