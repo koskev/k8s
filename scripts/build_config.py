88d3c6bf89c0c1daf60c02d0e1f6f157ccfa3d37
Kevin <kevin@kokev.de>
2026-09-08 20:37:21 +0200

    chore(build): Support passing the input to the config

--- scripts/build_config.py
+++ scripts/build_config.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
 import argparse
+import subprocess
 import os
 
 parser = argparse.ArgumentParser("config_gen")
 parser.add_argument("configs", help="Path to all config files", type=str, nargs="+")
 args = parser.parse_args()
+
+
+def get_import_type(import_file: str):
+    result = subprocess.run(f"echo \"std.type(import '{import_file}')\" | jsonnet -S -", shell=True, capture_output=True, text=True)
+    return result.stdout.strip()
 
 
 tree = {}
@@ -16,16 +22,22 @@ for config in args.configs:
     for part in parts:
         part = part.replace("-", "_")
         t = t.setdefault(f"{part}+", {})
-    t[f"config+"] = f"(import '{config}')"
+    import_type = get_import_type(config)
+    if import_type == "function":
+        t[f"config+"] = f"(import '{config}')(outerSelf)"
+    else:
+        t[f"config+"] = f"(import '{config}')"
 
-def tree_to_string(tree) -> str:
+def tree_to_string(tree, isRoot) -> str:
     if isinstance(tree, dict):
         vals = "{"
+        if isRoot:
+            vals += "local outerSelf = self,\n"
         for key,val in tree.items():
-            vals += f" {key}: {tree_to_string(val)},\n"
+            vals += f" {key}: {tree_to_string(val, False)},\n"
         vals += "}"
         return vals
     else:
         return(str(tree))
-print(tree_to_string(tree))
+print(tree_to_string(tree, True))
 
